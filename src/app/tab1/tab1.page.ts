5ae92fd5205fec5bb2ba988a7ce9a83a9afe4917
Stefani Totokotsopoulou <s.totokotsopoulou@epg-financials.com>
2023-10-19 10:39:02 +0300

    first wizard

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import {
+  InAppBrowser,
+  InAppBrowserOptions,
+} from "@ionic-native/in-app-browser/ngx";
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
+  openSonio() {
+    throw new Error("Method not implemented.");
+  }
 
-  constructor() {}
-
+  constructor(private iab: InAppBrowser) {}
 }
