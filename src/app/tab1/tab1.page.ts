7c2d8aed370cc7a434531dc95934b2b7116cd2d2
Stefani Totokotsopoulou <s.totokotsopoulou@epg-financials.com>
2023-10-20 10:19:39 +0300

    first wizard

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,35 @@
 import { Component } from "@angular/core";
 import { Browser } from "@capacitor/browser";
-
+import {
+  InAppBrowser,
+  InAppBrowserOptions,
+} from "@ionic-native/in-app-browser/ngx";
 @Component({
   selector: "app-tab1",
   templateUrl: "tab1.page.html",
   styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-  async openSonio() {
-    console.log("open sonio");
+  url = "https://staging.id.sonio-group.com/";
 
-    await Browser.open({
-      url: "https://staging.id.sonio-group.com/start/en/647efca334a855cf37cf58c3?session=652f9278b180801d2d11d708&flowid=647efd9734a855cf37cf58c5",
-    });
+  async openSonio() {
+    let defaultOptions: InAppBrowserOptions = {
+      clearsessioncache: "yes",
+      allowInlineMediaPlayback: "yes",
+      enableViewportScale: "no",
+      hidenavigationbuttons: "yes",
+      disallowoverscroll: "no",
+      usewkwebview: "yes",
+      presentationstyle: "fullscreen",
+      fullscreen: "yes",
+      zoom: "no",
+      location: "no",
+      toolbar: "no",
+    };
+
+    console.log("open sonio");
+    const browser = this.iab.create(this.url, "_blank", defaultOptions);
   }
 
-  constructor() {}
+  constructor(private iab: InAppBrowser) {}
 }
